8b11585107dc65afe0c19799a414c05656c359f7
jn23 <tehjawn@gmail.com>
2017-03-06 00:02:40 -0500

    Tutorial In Prog

--- old/src/aug.js
+++ old/src/aug.js
@@ -1,28 +1,13 @@
+// Aug.JS - JS Framework to create augmentable website building blocks
+// John Nguyen 2017
+
+'use strict';
+
 // Primary Aug Object that stores Aug Root Elements and their Shadow Root
 function Aug (rootElement) {
   this.rootElement = rootElement;
   this.shadowRoot = null;
   return this
-}
-
-// Defines a custom Aug Component
-Aug.prototype.component = function(targetElement, componentName) {
-  var newComponent = document.registerElement("aug-"+componentName, {
-    prototype: Object.create(HTMLElement.prototype, {
-      createdCallback: {
-        value: function() {
-          var root = this.createShadowRoot();
-          var template = document.querySelector(targetElement);
-          if (!template) {
-            var link = document.querySelector('link[rel=import][name='+componentName+']');
-            template = link.import.querySelector(targetElement);
-          }
-          var clone = document.importNode(template.content, true);
-          root.appendChild(clone);
-        }
-      }
-    })
-  });
 }
 
 // Sets the Root of the MAIN Aug Object
@@ -39,7 +24,7 @@ Aug.prototype.setView = function(view, aug) {
   // console.log("Setting Root View to: " + view);
 
 	// this.shadowRoot = this.rootElement.createShadowRoot();
-	this.shadowRoot = this.rootElement.createShadowRoot({mode:'open'});
+	this.shadowRoot = this.rootElement.attachShadow({mode:'open'});
   this.shadowRoot.appendChild(document.importNode(template.content, true));
 
   return this;
